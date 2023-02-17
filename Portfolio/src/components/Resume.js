6f8eb0d84640770c383322318d15286c6a7f78c1
CassNoisette <cassandrebeauzil@gmail.com>
2023-02-16 20:41:04 -0500

    updated all files

---
+++ Portfolio/src/components/Resume.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Resume() {
+    return(
+        <section className="center">
+            <h2>Resume</h2>
+            <div>
+               
+                <h2>Front-End</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>jQuery</li>
+                    <li></li>
+                </ul>
+                <h2>Back-End</h2>
+                <ul>
+                    <li>Nodejs</li>
+                    <li>MongoDB</li>
+                    <li>MySQL</li>
+                    <li>Express</li>
+                   
+                </ul>
+
+            </div>
+        </section>
+    )
+}
+
+export default Resume;
